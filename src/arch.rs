d1cd233b41af4ba987b1841da05cd235b3732a22
Gary Tierney <gary.tierney@gmx.com>
2018-06-06 23:03:32 +0100

    Add basic disassembly output for supported architectures
    At the moment the output is somewhat compatible with JitWatch, but does not
    include instruction type information so not all compiled code is reported
    correctly.

--- src/arch.rs
+++ src/arch.rs
@@ -1,15 +1,47 @@
 use super::capstone::{Arch, Mode};
 
-pub fn current_arch() -> Option<(Arch, Mode)> {
-    let machine_tuple = if cfg!(target_arch = "x86_64") {
-        (Arch::X86, Mode::Mode64)
-    } else if cfg!(target_arch = "x86") {
-        (Arch::X86, Mode::Mode32)
-    } else if cfg!(target_arch = "arm") {
-        (Arch::ARM, Mode::Arm)
-    } else {
-        return None;
-    };
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct MachineDescriptor(Arch, Mode);
 
-    Some(machine_tuple)
+impl MachineDescriptor {
+    pub fn current() -> Option<Self> {
+        let machine_tuple = if cfg!(target_arch = "x86_64") {
+            MachineDescriptor(Arch::X86, Mode::Mode64)
+        } else if cfg!(target_arch = "x86") {
+            MachineDescriptor(Arch::X86, Mode::Mode32)
+        } else if cfg!(target_arch = "arm") {
+            MachineDescriptor(Arch::ARM, Mode::Arm)
+        } else {
+            return None;
+        };
+
+        Some(machine_tuple)
+    }
+
+    pub fn arch(&self) -> Arch {
+        self.0
+    }
+
+    pub fn mode(&self) -> Mode {
+        self.1
+    }
+}
+
+impl ToString for MachineDescriptor {
+    fn to_string(&self) -> String {
+        let name = match (self.0, self.1) {
+            (Arch::X86, Mode::Mode64) => "amd64",
+            (Arch::X86, Mode::Mode32) => "i386",
+            (Arch::ARM, Mode::Arm) => "arm",
+            _ => "unknown",
+        };
+
+        name.to_owned()
+    }
+}
+
+impl Default for MachineDescriptor {
+    fn default() -> Self {
+        MachineDescriptor::current().expect("Unable to determine current host machine")
+    }
 }
--- src/decoder.rs
+++ src/decoder.rs
@@ -1,52 +1,63 @@
-use arch::current_arch;
-use capstone::*;
-use capstone::{Arch, Insn, Mode};
+use capstone::prelude::{ArchDetail, Capstone};
+use capstone::{Arch, Insn, InsnDetail, Mode, NO_EXTRA_MODE};
+
+use arch::MachineDescriptor;
 
 pub struct InstructionDecoder {
-    /// The supported capstone [Arch]itecture that the decoder is running on.
-    arch: Arch,
-
-    /// The architecture specific [Mode], e.g., 32-bit, 64-bit.
-    mode: Mode,
+    /// A descriptor of the machine disassembly is being generated for.
+    machine: MachineDescriptor,
 }
 
 impl InstructionDecoder {
-    fn new(host: (Arch, Mode)) -> Self {
-        InstructionDecoder {
-            arch: host.0,
-            mode: host.1
-        }
+    fn new(machine: MachineDescriptor) -> Self {
+        InstructionDecoder { machine }
     }
 }
 
 /// A callback trait that is invoked whenever an [InstructionDecoder] processes a new instruction.
 pub trait InstructionVisitor {
+    fn visit_begin(&mut self, machine: MachineDescriptor, addr: u64);
+
+    fn visit_end(&mut self, addr: u64);
+
     /// Visit the [instruction] from the given [arch] and [mode].
-    fn visit(&mut self, instruction: &Insn, arch: Arch, mode: Mode);
+    fn visit_insn(&mut self, instruction: &Insn, detail: &InsnDetail, arch: &ArchDetail);
 }
 
 impl InstructionDecoder {
-    pub fn decode<V>(&self, data: &[u8], visitor: &mut V)
+    pub fn decode<V>(&self, base_address: u64, data: &[u8], visitor: &mut V)
     where
         V: InstructionVisitor,
     {
-        let mut capstone =
-            Capstone::new_raw(self.arch, self.mode, NO_EXTRA_MODE, None).expect("//@todo");
+        let machine_name = self.machine.to_string();
+        let machine_arch = self.machine.arch();
+        let machine_mode = self.machine.mode();
 
-        let insns = capstone.disasm_all(data, 0).expect("@todo");
-        let iterator: InstructionIterator = insns.iter();
+        let mut cs =
+            Capstone::new_raw(machine_arch, machine_mode, NO_EXTRA_MODE, None).expect("//@todo");
 
-        for insn in iterator {
-            visitor.visit(&insn, self.arch, self.mode);
+        cs.set_detail(true).expect("@todo");
+
+        let instructions = cs.disasm_all(data, base_address as u64).expect("@todo");
+        let mut last_address = base_address;
+
+        visitor.visit_begin(self.machine, base_address);
+
+        for insn in instructions.iter() {
+            let insn_detail: InsnDetail = cs.insn_detail(&insn).unwrap();
+            let arch_detail: ArchDetail = insn_detail.arch_detail();
+
+            visitor.visit_insn(&insn, &insn_detail, &arch_detail);
+            last_address = insn.address();
         }
+
+        visitor.visit_end(last_address);
     }
 }
 
 /// Default [InstructionDecoder] that operates on machine code from the current architecture.
 impl Default for InstructionDecoder {
     fn default() -> Self {
-        InstructionDecoder::new(
-            current_arch().expect("Unable to detect current architecture.  Fatal error"),
-        )
+        InstructionDecoder::new(MachineDescriptor::default())
     }
 }
